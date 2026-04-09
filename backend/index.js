ef66b64fce97f2259719810e2c566a3726b56d4f
Muhammad Ansar <ansarjhedu@gmail.com>
2026-04-09 15:00:08 -0400

    backend changes

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
-import dotenv from "dotenv";
-dotenv.config();
-import dns from "dns";
-dns.setServers(['8.8.8.8', '8.8.4.4']);
+// import dotenv from "dotenv";
+// dotenv.config();
+// import dns from "dns";
+// dns.setServers(['8.8.8.8', '8.8.4.4']);
 
 import express from "express";
 import cors from "cors";
